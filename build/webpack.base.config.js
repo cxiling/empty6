ab742126058f583b2b5f2ebe1148e27cf863c3ec
cxiling <759564583@qq.com>
2016-11-23 09:43:32 +0800

    配置文件

---
+++ build/webpack.base.config.js
@@ -0,0 +1,53 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    path: './dist/js/',
+    publicPath: 'dist/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        // make sure to exclude 3rd party code in node_modules
+        exclude: /node_modules/
+      },
+      {
+        // edit this for additional asset file types
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url',
+        query: {
+          // inline files smaller then 10kb as base64 dataURL
+          limit: 10000,
+          // fallback to file-loader with this naming scheme
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  // vue-loader config:
+  // lint all JavaScript inside *.vue files with ESLint
+  // make sure to adjust your .eslintrc
+  vue: {
+    loaders: {
+      js: 'babel',
+      css: ExtractTextPlugin.extract("css"),
+      // you can also include <style lang="less"> or other langauges
+      less: ExtractTextPlugin.extract("css!less")
+    }
+  },
+  babel: {
+    presets: ['es2015'],
+    plugins: ['transform-runtime']
+  },
+  plugins: [
+    new ExtractTextPlugin("../css/style.css")
+  ]
+}
